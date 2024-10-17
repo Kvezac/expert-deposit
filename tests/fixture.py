2d234262e3e1d9bbdadacf3121bf18b4af3e35b9
Kvezac <Kvezak@gmail.com>
2024-10-17 16:54:48 +0300

    Changes to be committed: 	new file:   tests/fixture.py 	new file:   tests/test_deposit.py

---
+++ tests/fixture.py
@@ -0,0 +1,10 @@
+DATA = [
+    {"id": 1, "date": "31.01.2024", "periods": 3, "amount": 10_000, "rate": 6.4},
+    {"id": 2, "date": "27.02.2024", "periods": 3, "amount": 20_000, "rate": 6},
+    {"id": 3, "date": "31.03.2041", "periods": 60, "amount": 10_000, "rate": 6},
+    {"id": 4, "date": "31.01.2024", "periods": 5, "amount": 10_000, "rate": 6},
+    {"id": 5, "date": "31.01.2024", "periods": 3, "amount": 100_000, "rate": 6},
+    {"id": 6, "date": "31.01.2024", "periods": 3, "amount": 40_000, "rate": 6},
+    {"id": 7, "date": "27.01.2024", "periods": 3, "amount": 3_000_000, "rate": 6},
+    {"id": 8, "date": "10.10.2024", "periods": 3, "amount": 10_000, "rate": 1},
+]
